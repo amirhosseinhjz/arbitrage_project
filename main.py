45a8352b82fecec41b50fb991856fdccd077643d
amirhosseinhjz <amirhosseinhjz@outlook.com>
2022-07-02 18:18:03 +0430

    symbol translator added, commit for run on server

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import threading
 import orderbook.binance_orderbooks as binance
 import orderbook.kucoin as kucoin
 import time
 import pandas as pd
+from orderbook.symbol_translator import symboltranslator
+import threading
 symbols = ['BTC-USDT']#, 'ETH-USDT']
 kucoin_futures_symbs = ['XBTUSDTM']
 
@@ -16,20 +19,25 @@ api_secret = 'EYyaqoqRyPxozdDQQmlL7xQiHaqDIxgGAav68UoYAznTAOI6darEG132kavhI3Vh'
 
 
 class Main:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, pairs) -> None:
+        self.pairs = pairs
 
     def init_sockets(self):
-        self.binance_spot = binance.SpotOrderbookManager(api_key, api_secret, symbols)
+        self.binance_spot = binance.SpotOrderbookManager(api_key, api_secret, self.pairs)
         self.binance_spot.start()
-        self.binance_futures = binance.FuturesOrderbookManager(symbols)
+        self.binance_futures = binance.FuturesOrderbookManager(self.pairs)
         self.binance_futures.start()
-        self.kucoin_spot = kucoin.SpotOrderbookManager(key, secret, passphrase, symbols)
+        self.kucoin_spot = kucoin.SpotOrderbookManager(key, secret, passphrase, self.pairs)
         self.kucoin_spot.start()
-        self.kucoin_futures = kucoin.FuturesOrderbookManager(key, secret, passphrase, kucoin_futures_symbs)
+        self.kucoin_futures = kucoin.FuturesOrderbookManager(key, secret, passphrase, self.pairs)
         self.kucoin_futures.start()
 
-    def get_diffs_periodic(self):
+    def get_diffs_periodic(self, pair):
+        binance_spot_symbol = symboltranslator('binance', pair[0], pair[1], 'spot')
+        binance_futures_symbol = symboltranslator('binance', pair[0], pair[1], 'perp')
+        kucoin_spot_symbol = symboltranslator('kucoin', pair[0], pair[1], 'spot')
+        kucoin_futures_symbol = symboltranslator('kucoin', pair[0], pair[1], 'perp')
+
         times = []
         binance_spot_bid = []
         binance_spot_ask = []
@@ -44,23 +52,18 @@ class Main:
         while True:
             # print('Working...')
             t = int(time.time())
-            for j in range(len(symbols)):
-                symbol = symbols[j]
-                kucoin_fu_symbol = kucoin_futures_symbs[j]
-                binance_spot_bid.extend(self.binance_spot.get(symbol.replace('-', '')).bids)
-                binance_spot_ask.extend(self.binance_spot.get(symbol.replace('-', '')).asks)
-                binance_futures_bid.extend(self.binance_futures.get(symbol.replace('-', '')).bids)
-                binance_futures_ask.extend(self.binance_futures.get(symbol.replace('-', '')).asks)
-                kucoin_spot_bid.extend(self.kucoin_spot.get(symbol).bids)
-                kucoin_spot_ask.extend(self.kucoin_spot.get(symbol).asks)
-                kucoin_futures_bid.extend(self.kucoin_futures.get(kucoin_fu_symbol).bids)
-                kucoin_futures_ask.extend(self.kucoin_futures.get(kucoin_fu_symbol).asks)
+            binance_spot_bid.extend(self.binance_spot.get(binance_spot_symbol).bids)
+            binance_spot_ask.extend(self.binance_spot.get(binance_spot_symbol).asks)
+            binance_futures_bid.extend(self.binance_futures.get(binance_futures_symbol).bids)
+            binance_futures_ask.extend(self.binance_futures.get(binance_futures_symbol).asks)
+            kucoin_spot_bid.extend(self.kucoin_spot.get(kucoin_spot_symbol).bids)
+            kucoin_spot_ask.extend(self.kucoin_spot.get(kucoin_spot_symbol).asks)
+            kucoin_futures_bid.extend(self.kucoin_futures.get(kucoin_futures_symbol).bids)
+            kucoin_futures_ask.extend(self.kucoin_futures.get(kucoin_futures_symbol).asks)
             times.append(t)
-            print(len(times))
-                # bids.append([t]+ binance_spot_bid+ binance_futures_bid+ kucoin_spot_bid)
-                # asks.append([t]+ binance_spot_ask+ binance_futures_ask+ kucoin_spot_ask)
-            if len(times) > 1000:
-                writer = pd.ExcelWriter(f'result{i}.xlsx', engine='xlsxwriter')
+            print(pair, ' : ', len(times))
+            if len(times) > 500:
+                writer = pd.ExcelWriter(binance_spot_symbol+f'_{i}.xlsx', engine='xlsxwriter')
                 pd.DataFrame(binance_spot_bid, columns=['time', 'price', 'size']).to_excel(writer, startcol=1, startrow=1, index=False)
                 pd.DataFrame(binance_spot_ask, columns=['time', 'price', 'size']).to_excel(writer, startcol=5, startrow=1, index=False)
                 pd.DataFrame(binance_futures_bid, columns=['time', 'price', 'size']).to_excel(writer, startcol=9, startrow=1, index=False)
@@ -98,10 +101,17 @@ class Main:
 
     def start(self):
         self.init_sockets()
-        time.sleep(10)
-        self.get_diffs_periodic()
+        time.sleep(30)
+        self.threads = []
+        for pair in self.pairs:
+            thread = threading.Thread(target=self.get_diffs_periodic, args=(pair,))
+            self.threads.append(thread)
+            thread.start()
+            time.sleep(1)
 
 
 if __name__ == '__main__':
-    main = Main()
+    pairs = [('ETH', 'USDT'), ('BAT', 'USDT'), ('ALICE', 'USDT'), ('ADA', 'USDT'), 
+        ('LINA', 'USDT'), ('EOS', 'USDT'), ('LIT', 'USDT'), ('RSR', 'USDT'), ('CELR', 'USDT'), ('UNI', 'USDT')]
+    main = Main(pairs=pairs)
     main.start()
